fbb26932841c4554beb1621a93de085034491e6f
Payton Boone <paytonboone30@gmail.com>
2023-01-26 14:18:57 -0700

    Javascript Started

---
+++ app.js
@@ -0,0 +1,46 @@
+const heroes = [
+    {
+        name: 'Slate Slabrock',
+        type: 'dwarf',
+        damage: 5,
+        health: 100
+    },
+    {
+        name: 'Flint Ironstag',
+        type: 'elf',
+        damage: 10,
+        health: 50
+    }
+]
+
+
+const boss = {
+    health: 100,
+    maxHealth: 100,
+    damage: 5,
+    level: 1
+}
+
+
+function drawHeroes()
+function drawMonster()
+
+
+function damageHeroes()
+function damageMonster()
+
+
+
+function reviveMonster()
+
+
+
+
+
+function buyPotion()
+function buySam()
+function buyJake()
+
+
+
+function resetGame()
